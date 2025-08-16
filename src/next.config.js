b88d3ead521bcedeedd60c1fbf4c1fc32d4c71d3
Wes Sonnenreich <wes@intersective.com>
2025-08-16 16:37:22 -0400

    fix prisma issue

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@prisma/client'],
+  serverExternalPackages: ['@prisma/client'],
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
